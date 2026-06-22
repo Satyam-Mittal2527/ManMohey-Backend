3319738d78bdf6ddccdc5a17efaeaa50a233b7d0
Satyam-Mittal2527 <mittal.satyam646@gmail.com>
2026-06-22 10:24:14 +0530

    Add Backend URL

--- app/routes/home_page_routes.py
+++ app/routes/home_page_routes.py
@@ -1,11 +1,14 @@
+import os
 from fastapi import APIRouter
 
-router = APIRouter(prefix = "/api/Home")
+router = APIRouter(prefix="/api/Home")
+
+BASE_URL = os.getenv("BASE_URL")
 
 @router.get("/PageHero")
 def get_home_page_hero():
     return {
         "title": "HomePage Hero Picture",
         "description": "Hero Banner for the Home Page",
-        "image_url": "http://localhost:8000/public/hero_image.webp"
+        "image_url": f"{BASE_URL}/public/hero_image.webp"
     }
