908275fd2b0be0d1d6d5f9d657b530f8d26f0414
Satyam-Mittal2527 <mittal.satyam646@gmail.com>
2026-07-11 21:58:39 +0530

    HeroImageLinking

--- app/routes/home_page_routes.py
+++ app/routes/home_page_routes.py
@@ -1,5 +1,6 @@
 import os
 from fastapi import APIRouter
+from app.db.supabase_client import supabase, supabase_admin
 
 router = APIRouter(prefix="/api/Home")
 
@@ -7,14 +8,28 @@ BASE_URL = os.getenv("BASE_URL")
 
 @router.get("/PageHero")
 def get_home_page_hero():
+    response = (
+        supabase_admin
+        .table("home_banners")
+        .select("image_path, button_link")
+        .eq("is_active", True)
+        .order("display_order")
+        .execute()
+    )
+    print("Response"+str(response.data))
+    images = [{}]
+
+    for row in response.data:
+        image_url = (
+            supabase.storage
+            .from_("website-assets")
+            .get_public_url(row["image_path"])
+        )
+        temp_dict = {
+            "image_url": image_url,
+            "button_link": row["button_link"]
+        }
+        images.append(temp_dict)
     return {
-        "title": "HomePage Hero Picture",
-        "description": "Hero Banner for the Home Page",
-        "images" : [
-            f"{BASE_URL}/public/hero_image1.png",
-            f"{BASE_URL}/public/hero_image2.png",
-            f"{BASE_URL}/public/hero_image1.png"
-        ]
-        
-        
+        "images": images
     }
